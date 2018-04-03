3007c43e7c0aa8ae7cd5d445db1a981802adc8e4
vithusel <vithu@live.com>
2018-04-04 00:16:45 +0100

    fixed all issues with update script. Ready for use in Live

--- Scripts/Work in Progress/update.sh
+++ Scripts/Work in Progress/update.sh
@@ -1,16 +1,31 @@
-Echo Refreshing availible updates
-sudo apt-get update 
+#!/bin/bash
 
-Echo Installing updates
-sudo apt-get install update -y
-sudo apt-get install upgrade -y
+TEXT_RESET='\e[0m'
+TEXT_YELLOW='\e[0;33m'
+TEXT_RED_B='\e[1;31m'
 
-Echo update Raspberry Pi Firmware
+sudo sudo apt-get update 
+echo -e $TEXT_YELLOW
+echo 'Refreshing availible updates'
+echo -e $TEXT_RESET
+
+sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
+echo -e $TEXT_YELLOW
+echo 'Installed updates'
+echo -e $TEXT_RESET
+
 sudo rpi-update
+echo -e $TEXT_YELLOW
+echo 'Updated Raspberry Pi Firmware'
+echo -e $TEXT_RESET
 
-Echo Pull latest changes from github
 cd /var/www/html
 git pull
+echo -e $TEXT_YELLOW
+echo 'update webserver files'
+echo -e $TEXT_RESET
 
-Echo Reboot system
 sudo reboot
+echo -e $TEXT_YELLOW
+echo 'Reboot system'
+echo -e $TEXT_RESET
