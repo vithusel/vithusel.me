628236b77352f3c5556c86700fdd51c075767b2e
vithusel <vithu@live.com>
2018-04-06 22:52:39 +0100

    enable clicking out of login box to exit

--- assets/js/main.js
+++ assets/js/main.js
@@ -347,6 +347,16 @@
 
 				});
 
+                var modal = document.getElementById('id01');
+
+                window.onclick = function(event) {
+                  if (event.target == modal) {
+                     modal.style.display = "none";
+    }
+}	
+				
+
 	});
+	
 
 })(jQuery);
