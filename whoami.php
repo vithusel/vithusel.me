ffac964d23e83c2613a2faecb33151eae3e22ba8
vithusel <vithu@live.com>
2018-05-31 14:55:39 +0100

    show PDF

--- whoami.php
+++ whoami.php
@@ -32,7 +32,7 @@ include_once "everypage/header.php";
 									<p>The Second part to my Career came in the form of a level 3 apprenticeship in Networking and hardware which was then followed by working as a network engineering / support contractor. Contracting allowed me to lightly dip my feet into various sectors withing IT, I have had the opportunity to talk to people like System Admins (Linux and Windows), Programmers and even a Devops Engineer. I used my time contracting to help build a picture of where i want to be and set in motion the tasks required to help me acieve my goals/ </p>
 									    <h1>What those goals are you ask?</h1>
 									<p>I want to be a Devops Engineer since i don't like being fixed to one sector and Devops engineers are required to work in a lot of different sectors such as scripting and sysadmin to do this i'm currently doing a level 4 Networking Apprenticeship, This involves learning Cisco networking as well as all the fundamentals to system security. Whilst doing this i am also self training php, CSS, Python, Bash and Powershell. Two of these im learning by actually creating something such as this Website which i will improve as my skills do. I also intend to train for AWS Admin, Google Cloud Admin, SQL Databases and do a Microsoft MCSA Certification. as well as learning Java and maybe some .</p>
-									<p> Most of the above things i don't want to do because it will get me good money or help me settle in life but because i want to be able to understand how things such as Cloud platforms work and to be able to tinker with my own linux servers and create Systems such as Home Automation for my owm home. 
+									<p> Most of the above things i don't want to do because it will get me good money or help me settle in life but because i want to be able to understand how things such as Cloud platforms work and to be able to tinker with my own linux servers and create Systems such as Home Automation for my own home. 
 								<h1>Have a look at my CV</h1>
                                  <!-- Trigger/Open The Modal -->
                                  <button id="myBtn">Open Modal</button>
@@ -43,11 +43,11 @@ include_once "everypage/header.php";
                                  <!-- Modal content -->
                                  <div class="modal-content">
                                  <span class="close">&times;</span>
-                                 <p>Some text in the Modal..</p>
+                                 <iframe src="http://docs.google.com/gview?url=http://vithusel.me/Files\Download\CV.pdf&embedded=true" style="width:600px; height:500px;" frameborder="0"></iframe>
                                  </div>
 
                                  </div>
-								 	<h3><a href="/Files/Download/CV.docx" class="inner">Download</a></h3>								
+								 	<h3><a href="/Files/Download/CV.pdf" class="inner">Download</a></h3>								
 								</div>
 
 							</section>
