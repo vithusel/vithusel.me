a799fef4c42ae33aab7dc694c5890984ac34c944
vithusel <vithu@live.com>
2018-06-07 16:08:33 +0100

    revert

--- assets/js/popupbox.js
+++ assets/js/popupbox.js
@@ -9,8 +9,13 @@ var span = document.getElementsByClassName("closepopup")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = if (screen.width <= 699) {
-document.location = "current-mobile.php";
+    document.location = "CV-mobile.php";
 }
+
+function() {
+    modal.style.display = "block";
+}
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
